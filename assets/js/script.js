d8772505532a772f3d273b35a8fc1c12fa173cdc
Keerti Chaudhary <17keerti@gmail.com>
2022-04-13 23:07:33 -0700

    solving merge conflict

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,47 @@
 // initial modal with have a laugh or random activity options
 // based on their choice they will be redirected to a new page
 //page 1 joke: 
-//  tabs for memes and jokes
-// saved memes/jokes on the left hand side
-$(document).ready(function () {
-  $('select').formSelect();
-});
+    //  tabs for memes and jokes
+    // saved memes/jokes on the left hand side
 
-var displaySavedItemEl = document.querySelector("#displaySavedItem");
 
-function getUserPreferences() {
-  var userPreference = {};
-  userPreference.joke = document.getElementById("joke").checked;
-  userPreference.activity = document.getElementById("activity").checked;
-  userPreference.education = document.getElementById("educational").checked;
-  userPreference.recreational = document.getElementById("recreational").checked;
-  userPreference.social = document.getElementById("social").checked;
-  userPreference.diy = document.getElementById("diy").checked;
-  userPreference.charity = document.getElementById("charity").checked;
-  userPreference.cooking = document.getElementById("cooking").checked;
-  userPreference.relaxation = document.getElementById("relaxation").checked;
-  userPreference.music = document.getElementById("music").checked;
-  userPreference.busywork = document.getElementById("busywork").checked;
+    
+    $(document).ready(function(){
+        $('select').formSelect();
+      });
+  
+  var displaySavedItemEl = document.querySelector("#displaySavedItem");
+  
+  function getUserPreferences() {
+    var userPreference ={};
+    userPreference.education = document.getElementById("educational").checked ;
+    userPreference.recreational =document.getElementById("recreational").checked ;
+    userPreference.social =document.getElementById("social").checked ;
+    userPreference.diy =document.getElementById("diy").checked ;
+    userPreference.charity =document.getElementById("charity").checked ;
+    userPreference.cooking =document.getElementById("cooking").checked ;
+    userPreference.relaxation =document.getElementById("relaxation").checked ;
+    userPreference.music =document.getElementById("music").checked ;
+    userPreference.busywork =document.getElementById("busywork").checked ;
+  
+    console.log(userPreference);
+  }
+//     var allActivityTypes =  ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"]
+//     // convert this allActivityTypes var to only contain values user has selected
+//     // put random value from allActivityTypes to selectedActivity
+//     var selectedActivity = "recreational"
+//     var url = "http://www.boredapi.com/api/activity?type=" + selectedActivity ;
+//     // now fetch the url and show the result
+//     // if a check box equals true then we want it show
+// return
+//   }
+//   function getRandomInt(allActivityTypes.length) {
+//     return Math.floor(Math.random() * max);
+//   }
+//   pass allActivityTypes..index.length
 
-  console.log(userPreference);
-}
+
+
 
 
 
@@ -77,81 +94,40 @@ var displayRandomJoke = function (joke) {
     return;
   }
   var newEl = document.createElement('p');
-  newEl.innerHTML = `<p>Joke: ${joke.value} `
+  newEl.innerHTML=`<p>Joke: ${joke.value} `
   displaySavedItemEl.appendChild(newEl);
 }
 
 
 
 // Getting button to display content
+
 var contentBtn = document.getElementById("contentBtn")
 contentBtn.addEventListener("click", getContent)
-var displayedContent = document.getElementById("displaySavedItem")
-
 function getContent() {
-  getUserPreferences();
-  console.log("Getting Content")
-  displayedContent.classList.remove('hidden')
-  fetchAndDisplayRandomActivity();
-  fetchAndDisplayRandomJoke();
+    console.log("Getting Content")
+    displaySavedItemEl.classList.remove('hidden')
+    fetchAndDisplayRandomActivity()
+    fetchAndDisplayRandomJoke();
+    getUserPreferences();
 }
 
-
-var displayedContent = document.getElementById("displaySavedItem")
-
+var jokeTab = document.getElementById("jokeTab")
+jokeTab.addEventListener("click", getJokeContent)
 function getJokeContent() {
-  console.log("Getting Joke Content")
-  displayedContent.classList.remove('hidden')
-  fetchAndDisplayRandomJoke();
+    console.log("Getting Joke Content")
+    displaySavedItemEl.classList.remove('hidden')
+    fetchAndDisplayRandomJoke();
 }
 
-
-var displayedContent = document.getElementById("displaySavedItem")
-
+var activityTab = document.getElementById("activityTab")
+activityTab.addEventListener("click", getActivityContent)
 function getActivityContent() {
-  console.log("Getting Activity Content")
-  displayedContent.classList.remove('hidden')
-  fetchAndDisplayRandomActivity();
+    console.log("Getting Activity Content")
+    displaySavedItemEl.classList.remove('hidden')
+    fetchAndDisplayRandomActivity();
 
 }
-
-
-// assuming displayedItem = {
-//   joke_data: full joke API response
-//   activity_data: full activity API response
-// }
-function likeButtonHandler(displayedItem) {
-  var filledHeart = document.getElementById("heart");
-  var heartOutline = document.getElementById("heartOutline");
-  $(heartOutline).is(':visible');
-  var isLikeAction = true;
-  console.log("isLikedAction: " + isLikeAction);
-  // when user selects the heart
-  if (isLikeAction) {
-    // show filled heart and hide outlined heart
-    $(filledHeart).show();
-    $(heartOutline).hide();
-
-    var previousSavedItem = JSON.parse(localStorage.getItem("displayedItem"));
-    if (previousSavedItem == null) {
-      previousSavedItem = [];
-    }
-    previousSavedItem.push(displayedItem);
-    localStorage.setItem("userScores", JSON.stringify(previousSavedItem));
-
-  } else {
-
-    $(filledHeart).hide();
-    $(heartOutline).show();
-    // when user unselects the heart
-    // find the index of the displayedIndex in the previousSavedItem[] and delete that item and save the updated array.
-    var findDislikedItem = previousSavedItem.findIndex(Element => Element == displayedItem);
-    previousSavedItem.splice(findDislikedItem);
-    localStorage.setItem("userScores", JSON.stringify(previousSavedItem));
-  }
-
-// 
-
 
 
 
